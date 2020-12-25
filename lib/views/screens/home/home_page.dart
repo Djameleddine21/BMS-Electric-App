a993a07e7ab1c229459d88e75ca90f6729fa1fc8
Djameleddine21 <hd_sebbagh@esi.dz>
2020-12-25 12:06:34 +0100

    add google map widget

--- lib/views/screens/home/home_page.dart
+++ lib/views/screens/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatelessWidget {
   static const String id = '/home';
@@ -6,7 +7,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        body: SizedBox.expand(
+          child: GoogleMap(
+            initialCameraPosition: CameraPosition(target: LatLng(36.4, 3.2), zoom: 8.0),
+            onMapCreated: (_) {},
+          ),
+        ),
+      ),
     );
   }
 }

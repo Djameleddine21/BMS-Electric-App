f000517304dc98ae0361f16bea28433599c7051c
Djameleddine21 <hd_sebbagh@esi.dz>
2020-12-25 22:44:52 +0100

    add confirm page route

--- lib/views/routes/routes.dart
+++ lib/views/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:bms_electric/views/screens/add_reseller/add_reseller_page.dart';
 import 'package:bms_electric/views/screens/auth/auth_page.dart';
+import 'package:bms_electric/views/screens/confirm_code/confirm_code_page.dart';
 import 'package:bms_electric/views/screens/home/home_page.dart';
 import 'package:get/get.dart';
 
@@ -10,4 +11,6 @@ List<GetPage> pages = <GetPage>[
   GetPage(name: HomePage.id, page: () => HomePage()),
   // add reseller page
   GetPage(name: AddResellerPage.id, page: () => AddResellerPage()),
+  //confirm otp code page
+  GetPage(name: ConfirmCodePage.id, page: () => ConfirmCodePage()),
 ];

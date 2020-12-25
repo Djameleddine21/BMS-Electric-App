8072be8856a9a8c7712984ea8bfeae09c612f11f
djamelrassem <lamouridjamelrassem@gmail.com>
2020-12-25 22:13:13 +0100

    added edit reseller page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bms_electric/constants.dart';
 import 'package:bms_electric/views/routes/routes.dart';
 import 'package:bms_electric/views/screens/add_reseller/add_reseller_page.dart';
+import 'package:bms_electric/views/screens/edit_reseller/edit_reseller_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -30,7 +31,7 @@ class BMSApp extends StatelessWidget {
       ),
       defaultTransition: Transition.cupertino,
       getPages: pages,
-      initialRoute: AddResellerPage.id,
+      initialRoute: EditResellerPage.id,
     );
   }
 }

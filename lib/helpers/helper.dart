1a0193882dbdc2c64ec2b156d988f7f825e0e7e8
younes38 <hy_mimene@esi.dz>
2020-12-26 00:44:31 +0100

    added model to reseller profile

---
+++ lib/helpers/helper.dart
@@ -0,0 +1,12 @@
+String getActivityFromInt(int activity) {
+  if (activity == 1) {
+    return 'Quincaillerie';
+  }
+  if (activity == 2) {
+    return 'Electricien';
+  }
+  if (activity == 3) {
+    return 'Grossiste';
+  }
+  return '';
+}

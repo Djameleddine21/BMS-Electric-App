f8d295d7926007b28eaba5a97bdb142333d02091
Djameleddine21 <hd_sebbagh@esi.dz>
2020-12-26 00:46:40 +0100

    add reseller model

---
+++ lib/models/resseler/reseller.dart
@@ -0,0 +1,34 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'reseller.g.dart';
+
+@JsonSerializable()
+class Reseller {
+  String name;
+  String address;
+  String phone;
+  String wilaya;
+  String storeName;
+  String evaluation;
+  double rate;
+  double longitude;
+  double latitude;
+  int activity;
+
+  Reseller({
+    this.name,
+    this.address,
+    this.phone,
+    this.storeName,
+    this.wilaya,
+    this.evaluation,
+    this.rate,
+    this.longitude,
+    this.latitude,
+    this.activity,
+  });
+
+  factory Reseller.fromJson(Map<String, dynamic> json) => _$ResellerFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ResellerToJson(this);
+}
